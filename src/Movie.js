7d8c860ad6301f91ab90ce2385a6d39b3fde2c9d
feliciabergflo <81237992+feliciabergflo@users.noreply.github.com>
2022-08-18 00:30:28 +0200

    Lägger till CSS-filen som ska vara med i assignment 3a

--- src/Movie.js
+++ src/Movie.js
@@ -1,10 +1,18 @@
 import React from "react";
 
 export default function Movie(props) {
+
+    function getStars(stars) {
+        return "*".repeat(stars);
+    }
+
     return (
         <li className="list-group-item list-group-item-secondary my-1">
             { props.item.title }
             <button className="btn btn-sm btn-danger float-end" onClick={() => {props.deleteMovie(props.item.id)}}>X</button>
+            <p className="text-warning float-right">
+                { getStars(props.item.grade) }
+            </p>
         </li>
     )
 }
